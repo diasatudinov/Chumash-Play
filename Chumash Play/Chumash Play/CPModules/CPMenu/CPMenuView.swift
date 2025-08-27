f15ea62a5be2632efdd8f11c35d980ab47d8f2bb
Dias <dias.atudinov@gmail.com>
2025-08-27 17:46:45 +0500

    add splash view and main menu

--- Chumash Play/Chumash Play/CPModules/CPMenu/CPMenuView.swift
+++ Chumash Play/Chumash Play/CPModules/CPMenu/CPMenuView.swift
@@ -2,14 +2,112 @@
 //  CPMenuView.swift
 //  Chumash Play
 //
-//  Created by Dias Atudinov on 27.08.2025.
 //
 
 import SwiftUI
 
 struct CPMenuView: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+    @State private var showDailyReward = false
+    
+//    @StateObject var shopVM = JGShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+                
+                VStack(spacing: 0) {
+                    
+                    HStack {
+                        
+                        Button {
+                            showSettings = true
+                        } label: {
+                            Image(.settingsIconCP)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: JGDeviceManager.shared.deviceType == .pad ? 100:50)
+                        }
+                        
+                        Button {
+                            withAnimation {
+                                showDailyReward = true
+                            }
+                        } label: {
+                            Image(.dailyRewardsIconCP)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: JGDeviceManager.shared.deviceType == .pad ? 100:50)
+                        }
+                        
+                        Spacer()
+                        
+                        JGCoinBg()
+                    }.padding(20)
+                    Spacer()
+                    
+                    VStack(spacing: 7) {
+                        
+                        Button {
+                            showShop = true
+                        } label: {
+                            Image(.shopIconCP)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: JGDeviceManager.shared.deviceType == .pad ? 140:75)
+                        }
+                        
+                        Button {
+                            showGame = true
+                        } label: {
+                            Image(.playIconCP)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: JGDeviceManager.shared.deviceType == .pad ? 140:75)
+                        }
+                        
+                        Button {
+                            showAchievement = true
+                        } label: {
+                            Image(.achievementsIconCP)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: JGDeviceManager.shared.deviceType == .pad ? 140:75)
+                        }
+                        
+                    }
+                    Spacer()
+                }
+                                
+                
+                        
+        }.frame(maxWidth: .infinity)
+            .background(
+                ZStack {
+                    Image(.mainMenuBgCP)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+            )
+            .fullScreenCover(isPresented: $showGame) {
+//                GameRootView()
+            }
+            .fullScreenCover(isPresented: $showAchievement) {
+//                JGAchievementsView()
+            }
+            .fullScreenCover(isPresented: $showShop) {
+//                JGShopView(viewModel: shopVM)
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+//                JGSettingsView()
+            }
     }
 }
 
