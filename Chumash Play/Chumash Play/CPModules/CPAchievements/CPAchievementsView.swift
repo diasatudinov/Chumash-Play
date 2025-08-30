705dbf83c30476022981559f2e97a304f2f72259
Dias <dias.atudinov@gmail.com>
2025-08-30 15:13:15 +0500

    add achievements view

--- Chumash Play/Chumash Play/CPModules/CPAchievements/CPAchievementsView.swift
+++ Chumash Play/Chumash Play/CPModules/CPAchievements/CPAchievementsView.swift
@@ -2,14 +2,116 @@
 //  CPAchievementsView.swift
 //  Chumash Play
 //
-//  Created by Dias Atudinov on 29.08.2025.
 //
 
 import SwiftUI
 
 struct CPAchievementsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var achivementView = true
+    @StateObject var user = JGUser.shared
+        @Environment(\.presentationMode) var presentationMode
+
+        @StateObject var viewModel = NEGAchievementsViewModel()
+        @State private var index = 0
+        var body: some View {
+            ZStack {
+                Image(achivementView ? .achievementsBgCP: .dailyBgCP)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .onTapGesture {
+                        achivementView.toggle()
+                    }
+                
+                VStack {
+                    ZStack {
+                        HStack(alignment: .top) {
+                            
+                            Button {
+                                presentationMode.wrappedValue.dismiss()
+                                
+                            } label: {
+                                Image(.backBtnCP)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: JGDeviceManager.shared.deviceType == .pad ? 100:60)
+                            }
+                            
+                            Spacer()
+                            JGCoinBg()
+                        }
+                    }.padding([.top])
+                    
+                    ScrollView {
+                        if achivementView {
+                            VStack {
+                                
+                                ForEach(viewModel.achievements, id: \.self) { item in
+                                    achievementItem(item: item)
+                                        .onTapGesture {
+                                            viewModel.achieveToggle(item)
+                                            if !item.isAchieved {
+                                                user.updateUserMoney(for: 10)
+                                            }
+                                        }
+                                }
+                                
+                            }
+                        } else {
+                            VStack {
+                                
+                                ForEach(viewModel.dailyQuests, id: \.self) { item in
+                                    achievementItem(item: item)
+                                        .onTapGesture {
+                                            viewModel.questsToggle(item)
+                                            if !item.isAchieved {
+                                                user.updateUserMoney(for: 10)
+                                            }
+                                        }
+                                }
+                                
+                            }
+                        }
+                    }.padding()
+                    
+                }
+            }
+            .background(
+                ZStack {
+                    Image(.appBgCP)
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                    
+                    
+                }
+            )
+        }
+        
+    @ViewBuilder func achievementItem(item: NEGAchievement) -> some View {
+        HStack(spacing: 0) {
+            if !item.image.isEmpty {
+                Image(item.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: JGDeviceManager.shared.deviceType == .pad ? 100:60)
+            }
+            
+            Image(item.title)
+                .resizable()
+                .scaledToFit()
+                .frame(width: JGDeviceManager.shared.deviceType == .pad ? 300:245)
+            
+            Image(.claimBtnCP)
+                .resizable()
+                .scaledToFit()
+                .frame(height: JGDeviceManager.shared.deviceType == .pad ? 50:36)
+                .opacity(item.isAchieved ? 0:1)
+        }.opacity(item.isAchieved ? 1:0.5)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 5)
+            .background(.black.opacity(0.5))
+            .cornerRadius(4)
     }
 }
 
--- Chumash Play/Chumash Play/CPModules/CPAchievements/NEGAchievementsViewModel.swift
+++ Chumash Play/Chumash Play/CPModules/CPAchievements/NEGAchievementsViewModel.swift
@@ -1,16 +1,34 @@
+//
+//  NEGAchievementsViewModel.swift
+//  Chumash Play
+//
+//  Created by Dias Atudinov on 29.08.2025.
+//
+
+
 import SwiftUI
 
 class NEGAchievementsViewModel: ObservableObject {
     
     @Published var achievements: [NEGAchievement] = [
-        NEGAchievement(image: "achieve1ImageNEG", title: "Zombie party", subtitle: "There should be 15 zombies in one field." ,isAchieved: false),
-        NEGAchievement(image: "achieve2ImageNEG", title: "Brain Party", subtitle: "Collect 1000 brains" ,isAchieved: false),
-        NEGAchievement(image: "achieve3ImageNEG", title: "Full house at cemetery", subtitle: "Lose 1000 zombies" ,isAchieved: false),
-        NEGAchievement(image: "achieve4ImageNEG", title: "Five hundred dead", subtitle: "Kill 500 warriors" ,isAchieved: false),
+        NEGAchievement(image: "achieve1ImageCP", title: "achieve1TextCP", isAchieved: false),
+        NEGAchievement(image: "achieve2ImageCP", title: "achieve2TextCP", isAchieved: false),
+        NEGAchievement(image: "achieve3ImageCP", title: "achieve3TextCP", isAchieved: false),
+        NEGAchievement(image: "achieve4ImageCP", title: "achieve4TextCP", isAchieved: false),
 
     ] {
         didSet {
             saveAchievementsItem()
+        }
+    }
+    
+    @Published var dailyQuests: [NEGAchievement] = [
+        NEGAchievement(image: "", title: "quest1TextCP", isAchieved: false),
+        NEGAchievement(image: "", title: "quest2TextCP", isAchieved: false),
+        NEGAchievement(image: "", title: "quest3TextCP", isAchieved: false)
+    ] {
+        didSet {
+            saveDailyQuestsItem()
         }
     }
     
@@ -19,7 +37,8 @@ class NEGAchievementsViewModel: ObservableObject {
         
     }
     
-    private let userDefaultsAchievementsKey = "achievementsKeyNG"
+    private let userDefaultsAchievementsKey = "achievementsKeyCP"
+    private let userDefaultsDailyQuestsKey = "questsKeyCP"
     
     func achieveToggle(_ achive: NEGAchievement) {
         guard let index = achievements.firstIndex(where: { $0.id == achive.id })
@@ -27,6 +46,15 @@ class NEGAchievementsViewModel: ObservableObject {
             return
         }
         achievements[index].isAchieved.toggle()
+        
+    }
+    
+    func questsToggle(_ achive: NEGAchievement) {
+        guard let index = dailyQuests.firstIndex(where: { $0.id == achive.id })
+        else {
+            return
+        }
+        dailyQuests[index].isAchieved.toggle()
         
     }
     
@@ -46,12 +74,27 @@ class NEGAchievementsViewModel: ObservableObject {
             print("No saved data found")
         }
     }
+    
+    func saveDailyQuestsItem() {
+        if let encodedData = try? JSONEncoder().encode(dailyQuests) {
+            UserDefaults.standard.set(encodedData, forKey: userDefaultsDailyQuestsKey)
+        }
+        
+    }
+    
+    func loadDailyQuestsItem() {
+        if let savedData = UserDefaults.standard.data(forKey: userDefaultsDailyQuestsKey),
+           let loadedItem = try? JSONDecoder().decode([NEGAchievement].self, from: savedData) {
+            dailyQuests = loadedItem
+        } else {
+            print("No saved data found")
+        }
+    }
 }
 
 struct NEGAchievement: Codable, Hashable, Identifiable {
     var id = UUID()
     var image: String
     var title: String
-    var subtitle: String
     var isAchieved: Bool
 }
