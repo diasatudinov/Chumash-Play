320f8a46025df12c526439371661d866188270fa
Dias <dias.atudinov@gmail.com>
2025-08-30 15:13:26 +0500

    add daily rewards view

--- Chumash Play/Chumash Play/CPModules/CPDailyRewards/CPDailyRewardsView.swift
+++ Chumash Play/Chumash Play/CPModules/CPDailyRewards/CPDailyRewardsView.swift
@@ -2,16 +2,91 @@
 //  CPDailyRewardsView.swift
 //  Chumash Play
 //
-//  Created by Dias Atudinov on 29.08.2025.
 //
 
 import SwiftUI
 
 struct CPDailyRewardsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject private var viewModel = DailyRewardsViewModel()
+//    @StateObject var shopVM = NEGShopViewModel()
+    
+    private let columns = Array(repeating: GridItem(.flexible(), spacing: 12), count: 4)
+    private let dayCellHeight: CGFloat = JGDeviceManager.shared.deviceType == .pad ? 200:108
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 0) {
+                
+                ZStack {
+                    
+                    Image(.dailyRewardsBgCP)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        ForEach(1...viewModel.totalDaysCount, id: \.self) { day in
+                            ZStack {
+                                Image(.dayBgCP)
+                                    .resizable()
+                                    .scaledToFit()
+                                    
+                                VStack(spacing: 5) {
+                                    Text("Day \(day)")
+                                        .font(.system(size: 22, weight: .regular))
+                                        .foregroundStyle(.white)
+                                        .textCase(.uppercase)
+                                    
+                                    Button {
+                                        viewModel.claimNext()
+                                    } label: {
+                                        Image(.claimBtnCP)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(height: JGDeviceManager.shared.deviceType == .pad ? 70:38)
+                                    }
+                                    
+                                }.padding(20)
+                            }
+                            .frame(width: 105, height: dayCellHeight)
+                            .offset(x: day > 4 ? dayCellHeight/2:0)
+                            .opacity(viewModel.isDayClaimed(day) ? 1: viewModel.isDayUnlocked(day) ? 0.7:0.3)
+                            
+                        }
+                    }.frame(width: JGDeviceManager.shared.deviceType == .pad ? 800:350)
+                }
+            }.padding()
+            
+            VStack {
+                HStack(alignment: .top) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                        
+                    } label: {
+                        Image(.backBtnCP)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: JGDeviceManager.shared.deviceType == .pad ? 100:50)
+                    }
+                    Spacer()
+                    
+                    JGCoinBg()
+                }.padding([.horizontal, .top])
+                
+                Spacer()
+            }
+            
+        }.background(
+            ZStack {
+                Image(.appBgCP)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            }
+        )
     }
 }
+
 
 #Preview {
     CPDailyRewardsView()
